1cad05ab6a48e60e37dbc159e4be1ed472492446
Oliver Kroener <ok@oliver-kroener>
2026-02-14 22:28:47 +0100

    [TASK] Fixed issue with CKE

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,10 @@ defined('TYPO3') || defined('TYPO3_MODE') || die();
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ok_ai_writer'] = 'EXT:ok_ai_writer/Configuration/RTE/Default.yaml';
 
 // TYPO3 10 only: override RTE preset via Page TSconfig
-if (\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class)->getMajorVersion() < 11) {
+$_okAiWriterTypo3Version = new \TYPO3\CMS\Core\Information\Typo3Version();
+if ($_okAiWriterTypo3Version->getMajorVersion() < 11) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         'RTE.default.preset = ok_ai_writer'
     );
 }
+unset($_okAiWriterTypo3Version);
