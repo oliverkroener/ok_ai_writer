ec43400bf6362a8f90b444cf195d2d362033501d
Oliver Kroener <ok@oliver-kroener>
2026-02-14 23:16:10 +0100

    [TASK] Updated icons

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'ext-ok-ai-writer' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:ok_ai_writer/Resources/Public/Icons/Extension.svg',
+    ],
+];
